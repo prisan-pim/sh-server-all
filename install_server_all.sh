d4a4ab2ee541547048243546d6d12b56be7628f1
prisan6046 <prisan_big_bboy@hotmail.com>
2019-02-14 00:30:27 +0700

    new file

--- install_server_all.sh
+++ install_server_all.sh
@@ -16,7 +16,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 #apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
-sudo systemctl status docker
+#sudo systemctl status docker
 echo "*** Install Docker ok..."
 
 echo "*** 2.Docker-compose"
