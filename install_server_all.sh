5b63210229ede93025dc543e1b40f2203f661216
prisan6046 <prisan_big_bboy@hotmail.com>
2019-02-14 00:23:41 +0700

    new file

---
+++ install_server_all.sh
@@ -0,0 +1,36 @@
+echo "*** Welcome to Sotfware All server ***"
+echo "***           Server all           ***"
+echo "*** 1.Nginx"
+echo "*** 2.Docker"
+echo "*** 3.Docker-compose\n"
+
+echo "*** 1.Nginx"
+sudo apt-get update
+sudo apt-get install nginx
+sudo ufw app list
+echo "*** Install Nginx ok..."
+
+echo "*** 2.Docker"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+echo "*** Install Docker ok..."
+
+echo "*** 2.Docker-compose"
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+echo "*** Install Docker ok..."
+
+
+echo "*** start docker nginx ***"
+export WORKDIR=$(pwd)
+
+echo "\n############### start docker Nginx  ###############"
+docker-compose -f ${WORKDIR}/server-nginx/docker-compose.yml up -d
+
+echo "\n*** remove docker none all ***"
+docker rmi $(docker images | grep '<none>' | awk {'print$3'})
