ba371ad000796e2d64d8dfe8ddfe43858b1b885c
prisan6046 <prisan6046@users.noreply.github.com>
2019-10-03 17:01:06 +0700

    Update install_server_all.sh

--- install_server_all.sh
+++ install_server_all.sh
@@ -18,7 +18,8 @@ sudo apt-get install -y docker-ce
 echo "*** Install Docker ok..."
 
 echo "*** 2.Docker-compose"
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 echo "*** Install Docker ok..."
